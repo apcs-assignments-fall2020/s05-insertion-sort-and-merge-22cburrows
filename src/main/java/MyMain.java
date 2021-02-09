505b49506a157652dee8046bba21e4b51ff16906
22cburrows <22cburrows@athenian.org>
2021-02-09 02:17:28 +0000

    merge two ints compleated

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -12,8 +12,32 @@ public class MyMain {
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int[] end = new int[arr1.length + arr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while ((i < arr1.length) && ( j < arr2.length)){
+            if (arr1[i] < arr2[j]){
+                end[k] = arr1[i];
+                i++;
+            }
+            else{
+                end[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < arr1.length){
+            end[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < arr2.length){
+            end[k] = arr2[j];
+            j++;
+            k++;
+        }
+        return end;
     }
 
     public static void main(String[] args) {
